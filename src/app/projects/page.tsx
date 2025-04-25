1c25ce4e489cb85a10967a6299ed79b42817c543
mrocher <apostlepro1@gmail.com>
2025-04-25 13:35:19 +0200

    ajout minishell & modif des photos 42projet et minishell pour voir le haut

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Projects() {
-  // Données des projets (normalement, ces données pourraient venir d'une API ou d'un CMS)
   const projects = [
     {
       id: 'serveur-irc',
@@ -52,6 +51,15 @@ export default function Projects() {
       featured: false
     },
     {
+      id: 'minishell',
+      title: 'Un minishell en C',
+      description: 'Un shell Unix minimaliste, conçu pour être léger et rapide.',
+      image: '/minishell.png',
+      tags: ['C', 'Shell', 'Unix'],
+      github: 'https://github.com/0xAp0stl3/minishell',
+      featured: false
+    },
+    {
       id: '42projects',
       title: 'Projets 42',
       description: 'Une collection de projets réalisés dans le cadre de ma formation à 42.',
@@ -87,7 +95,10 @@ export default function Projects() {
                     src={project.image}
                     alt={project.title}
                     fill
-                    style={{ objectFit: "cover" }}
+                    style={
+                      { objectFit: "cover",
+                        objectPosition: (project.id === "minishell" || project.id === "42projects") ? "top" : "center"
+                      }}
                   />
                   {project.featured && (
                     <div className="absolute top-4 right-4 bg-blue-600 text-white text-xs font-bold px-3 py-1 rounded-full">
@@ -121,12 +132,6 @@ export default function Projects() {
                         <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
                       </svg>
                     </a>
-                    {/* <Link 
-                      href={`/projects/${project.id}`}
-                      className="text-blue-600 dark:text-blue-400 hover:underline font-medium"
-                    >
-                      Voir les détails →
-                    </Link> */}
                   </div>
                 </div>
               </div>

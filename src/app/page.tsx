8c8ed46615ce8146865140e7505220dd19832b52
mrocher <apostlepro1@gmail.com>
2025-04-27 13:55:55 +0200

    change photo profil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,10 +34,10 @@ export default function Home() {
             <div className="flex justify-center">
               <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white dark:border-gray-800 shadow-lg">
                 <Image
-                  src="/profil.jpeg"
+                  src="/profil_ia.png"
                   alt="Photo de profil"
                   fill
-                  style={{ objectFit: "cover" }}
+                  style={{ objectFit: "cover", objectPosition: "center top" }}
                   priority
                 />
               </div>
